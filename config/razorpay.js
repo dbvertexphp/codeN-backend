08b6127f79a093059e109d98fde56406dd206761
ddb58540-dot <ddb58540@gmail.com>
2026-03-07 15:00:45 +0530

    test

--- config/razorpay.js
+++ config/razorpay.js
@@ -1,8 +1,22 @@
+// import Razorpay from "razorpay";
+
+// const razorpayInstance = new Razorpay({
+//   key_id: "rzp_test_SJTKjbIuvQ1Z5X",
+//   key_secret: "leMWrmw1vvOr16wiXseND9qX",
+// });
+
+// console.log("Razorpay Initialized ✅");
+
+// export default razorpayInstance;
+
 import Razorpay from "razorpay";
+import dotenv from "dotenv";
+
+dotenv.config(); // 🔥 important
 
 const razorpayInstance = new Razorpay({
-  key_id: "rzp_test_SJTKjbIuvQ1Z5X",
-  key_secret: "leMWrmw1vvOr16wiXseND9qX",
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
 console.log("Razorpay Initialized ✅");
